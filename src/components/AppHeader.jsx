e7eafa9bc732e1420a42a98fc434d3a83411e516
yewyewXD <yewyew6933@gmail.com>
2022-03-24 16:03:04 +0800

    setup moralis

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,33 +1,9 @@
 import React from "react";
 import NavMenu from "./NavMenu";
 import "../styles/components/header.css";
-import Web3 from "web3";
-import Web3Modal from "web3modal";
 
 const AppHeader = () => {
-  async function connectWallet() {
-    const providerOptions = {
-      injected: {
-        display: {
-          logo: "data:image/gif;base64,INSERT_BASE64_STRING",
-          name: "Injected",
-          description: "Connect with the provider in your Browser",
-        },
-        package: null,
-      },
-    };
-
-    const web3Modal = new Web3Modal({
-      network: "mainnet", // optional
-      cacheProvider: true, // optional
-      providerOptions, // required
-      theme: "dark",
-    });
-
-    const provider = await web3Modal.connect();
-
-    const web3 = new Web3(provider);
-  }
+  async function connectWallet() {}
 
   return (
     <>
@@ -44,7 +20,7 @@ const AppHeader = () => {
       </div>
 
       <div class="wallet-holder">
-        <button type="button" class="wallet-button" onClick={connectWallet}>
+        <button type="button" onClick={connectWallet}>
           <div className="if-connected">
             {/* {{wallet.userAddress.substring(0, 14)}} */}
           </div>
--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { MoralisProvider } from "react-moralis";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MoralisProvider
+      appId={process.env.REACT_APP_MORALIS_APP}
+      serverUrl={process.env.REACT_APP_MORALIS_SERVER}
+    >
+      <App />
+    </MoralisProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
