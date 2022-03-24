850809c330346643313b3ab1d2f73dfe60763bd1
yewyewXD <yewyew6933@gmail.com>
2022-03-24 16:38:08 +0800

    update meta-title, snake page space

--- src/components/SnakeGame.jsx
+++ src/components/SnakeGame.jsx
@@ -4,6 +4,7 @@ import Snake from "react-simple-snake";
 const SnakeGame = () => {
   return (
     <main>
+      <div className="mt-8 w-full"></div>
       <Snake snakeColor="#6081a9" appleColor="#66dfcd" percentageWidth={50} />
     </main>
   );
