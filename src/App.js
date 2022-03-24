82b3ee712d0529a6eb291ca73ffeaa6bdf3a69bd
yewyewXD <yewyew6933@gmail.com>
2022-03-24 10:56:53 +0800

    setup react-router v6

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function Test() {
+  return <div>heelo</div>;
+}
+
 function App() {
-  return <div>testing</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/about" element={<Test />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
