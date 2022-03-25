161b29d721fd8211b02e56a3f4359b98d8178895
yewyewXD <yewyew6933@gmail.com>
2022-03-25 08:20:23 +0800

    add bot recaptcha

--- src/components/SnakeGame.jsx
+++ src/components/SnakeGame.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Snake from "react-simple-snake";
+import HCaptcha from "@hcaptcha/react-hcaptcha";
 
 const SnakeGame = () => {
+  const [hasVerified, setHasVerified] = useState(false);
+
   return (
     <main>
       <div className="mt-8 w-full"></div>
-      <Snake snakeColor="#6081a9" appleColor="#66dfcd" percentageWidth={50} />
+      {hasVerified ? (
+        <Snake snakeColor="#6081a9" appleColor="#66dfcd" percentageWidth={50} />
+      ) : (
+        <div className="flex justify-center items-center h-80">
+          <HCaptcha
+            sitekey={process.env.REACT_APP_RECAPTCHA_KEY}
+            onVerify={(token, ekey) => {
+              setTimeout(() => {
+                setHasVerified(true);
+              }, 1000);
+            }}
+          />
+        </div>
+      )}
     </main>
   );
 };
