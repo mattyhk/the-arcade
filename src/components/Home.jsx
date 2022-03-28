e9de56d5e3cb9a97f2e1942565cd98ca7f62af4e
yewyewXD <yewyew6933@gmail.com>
2022-03-29 02:42:37 +0800

    fix margins

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,9 +12,7 @@ const Hero = () => {
 
   return (
     <main>
-      <div className="cursed-text header">
-        The Arcade
-      </div>
+      <div className="cursed-text header">The Arcade</div>
 
       <div className="two-cols container">
         <div>
@@ -24,13 +22,20 @@ const Hero = () => {
           </div>
           <div className="text">
             <p>
-              Don't got the money but have the skill? Get the top score and get the NFT. 
+              Don't got the money but have the skill? Get the top score and get
+              the NFT.
             </p>
             <p>
-              No more WL grind, no more gas wars. It's time we socialized NFTs and make web3 fun again. May the best player win!
+              No more WL grind, no more gas wars. It's time we socialized NFTs
+              and make web3 fun again. May the best player win!
             </p>
             <p>
-              Each week is a new tournament with one NFT up for grabs. Top score at the end wins the NFT - play as many times as you want. Top 100 of the leaderboard (except #1) splits 10% of the entry funds raised proportionally based on their score. 60% of funds go to the NFT provider, the rest goes to the community. Put your money where your mouth is and compete to win in <strong>The Arcade.</strong>
+              Each week is a new tournament with one NFT up for grabs. Top score
+              at the end wins the NFT - play as many times as you want. Top 100
+              of the leaderboard (except #1) splits 10% of the entry funds
+              raised proportionally based on their score. 60% of funds go to the
+              NFT provider, the rest goes to the community. Put your money where
+              your mouth is and compete to win in <strong>The Arcade.</strong>
             </p>
           </div>
         </div>
@@ -74,7 +79,7 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className="button-holder centered opensea-bar">
+      <div className="button-holder centered opensea-bar mb-12">
         <button className="opensea-button">
           <a
             href="/snake"
@@ -87,14 +92,11 @@ const Hero = () => {
         </button>
       </div>
 
-      <div className="one-col container" style={{margintop: "100px"}}>
+      <div className="one-col container" style={{ margintop: "100px" }}>
         <div className="text-wrapper">
-          <div className="centered sub-header">
-            How To
-          </div>
+          <div className="centered sub-header">How To</div>
         </div>
       </div>
-
 
       <div className="two-cols container on-chain-section">
         <div className="text-wrapper">
@@ -103,9 +105,16 @@ const Hero = () => {
           </div>
           <div className="text">
             <ol>
-              <li>1. Purchase Play Tokens - buy one or many. Mint and play, or mint and hold. All designed to reduce gas for you.</li>
+              <li>
+                1. Purchase Play Tokens - buy one or many. Mint and play, or
+                mint and hold. All designed to reduce gas for you.
+              </li>
               <li>2. Spend a token and play a round.</li>
-              <li>3. Top score wins the NFT. All others earn a piece of the prize pool - that's right, that's ETH going into your wallet just for playing.</li>
+              <li>
+                3. Top score wins the NFT. All others earn a piece of the prize
+                pool - that's right, that's ETH going into your wallet just for
+                playing.
+              </li>
             </ol>
           </div>
         </div>
@@ -117,14 +126,17 @@ const Hero = () => {
           <div className="text">
             <ol>
               <li>1. Choose the NFT you want to put up for grabs.</li>
-              <li>2. Set your floor - if the entry pool doesn't hit it, the NFT is returned to you.</li>
+              <li>
+                2. Set your floor - if the entry pool doesn't hit it, the NFT is
+                returned to you.
+              </li>
               <li>3. End of the tournament - collect your ETH. That's it.</li>
             </ol>
           </div>
         </div>
       </div>
 
-      <div className="button-holder centered opensea-bar">
+      <div className="button-holder centered opensea-bar mb-12">
         <button className="view-button">
           <a
             href="https://twitter.com"
@@ -155,18 +167,24 @@ const Hero = () => {
           </div>
           <div className="text">
             <p>
-              We believe in making the NFT space fun again. No more long WL process, fighting with bots and going through the same steps for every project. It's time to reward your digital skill with digital rewards. 
+              We believe in making the NFT space fun again. No more long WL
+              process, fighting with bots and going through the same steps for
+              every project. It's time to reward your digital skill with digital
+              rewards.
             </p>
             <p>
-              Tournaments of your favorite games, with NFTs up for grabs as grand prizes and a share of the prize pool for the rest of the leaderboard. 
+              Tournaments of your favorite games, with NFTs up for grabs as
+              grand prizes and a share of the prize pool for the rest of the
+              leaderboard.
             </p>
             <p>
-              For NFT holders - earn more than the open market by listing your NFT to win. You set the game, the floor and walk away with the lion's share of the entry pool.
+              For NFT holders - earn more than the open market by listing your
+              NFT to win. You set the game, the floor and walk away with the
+              lion's share of the entry pool.
             </p>
           </div>
         </div>
       </div>
-
     </main>
   );
 };
